bb8d4e57ce3e3e8116970844e4620d6720f81f4c
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-04-01 14:18:16 +0200

    Update SpawnSwarmtcK.sh

--- SpawnSwarmtcK.sh
+++ SpawnSwarmtcK.sh
@@ -295,7 +295,7 @@ echo ""
 #KILLS SWARM (Testing purposes cleanup)
 docker-machine rm swarm-master
 docker-machine rm SPAWN-CONSUL
-docker-machine rm SPAWN-receiver
+docker-machine rm spawn-receiver
 docker-machine rm SPAWN-FigureITOUT
 
 
