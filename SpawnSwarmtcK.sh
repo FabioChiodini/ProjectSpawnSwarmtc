8fa31dc8938f4229f564f6676aa7b4ed1871b015
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-04-04 13:31:45 +0200

    Update SpawnSwarmtcK.sh
    Total GCEs and others

--- SpawnSwarmtcK.sh
+++ SpawnSwarmtcK.sh
@@ -58,6 +58,11 @@ publicipCONSULK=$(docker-machine ip SPAWN-CONSUL)
 docker run -d -p 8400:8400 -p 8500:8500 -p 8600:53/udp -h node1 progrium/consul -server -bootstrap
 
 
+#Register the tasks for this run in Consul
+curl -X PUT -d 'Total Slaves in AWS' http://$publicipCONSULK:8500/v1/kv/tc/AWSVMs/$VM_InstancesK/key?flags=1
+curl -X PUT -d 'Total Slaves in GCE' http://$publicipCONSULK:8500/v1/kv/tc/GCEVMs/$GCEVM_InstancesK/key?flags=1
+curl -X PUT -d 'Total Honeypots' http://$publicipCONSULK:8500/v1/kv/tc/TotalHoneypots/$Container_InstancesK/key?flags=1
+
 echo ----
 echo Consul RUNNING ON $publicipCONSULK:8500
 echo publicipCONSULK=$publicipCONSULK
