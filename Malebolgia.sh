48dcf79671a7081bd97aacbdd6f820c18b748663
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-04-09 13:52:12 +0200

    Create Malebolgia.sh
    Teardown script

---
+++ Malebolgia.sh
@@ -0,0 +1,13 @@
+#Spawn Tear down script
+#Cleans up the whole envinronment by reading information from etcd
+#It also uses /home/ec2-user/Cloud1 parameters
+
+
+
+#Read data from etcd
+
+
+#Kill Docker Machines
+
+
+#Kill local containers
