5da999f5aabfe876930950aada53a6f1f8873d7f
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-04-09 14:37:48 +0200

    Update Malebolgia.sh

--- Malebolgia.sh
+++ Malebolgia.sh
@@ -31,11 +31,11 @@ prevgcevms=`(curl http://127.0.0.1:4001/v2/keys/gcevms | jq '.node.value' | sed 
 prevhoneypots=`(curl http://127.0.0.1:4001/v2/keys/totalhoneypots | jq '.node.value' | sed 's/.//;s/.$//')`
 
 #swarm-master
-publicipSWARMK=`(http://127.0.0.1:4001/v2/keys/swarm-master/ip | jq '.node.value' | sed 's/.//;s/.$//')`
+publicipSWARMK=`(curl http://127.0.0.1:4001/v2/keys/swarm-master/ip | jq '.node.value' | sed 's/.//;s/.$//')`
 SwarmTokenK=`(curl http://127.0.0.1:4001/v2/keys/swarm-master/token | jq '.node.value' | sed 's/.//;s/.$//')`
 
 #SPAWN_CONSUL
-ConsulVMNameK=`(http://127.0.0.1:4001/v2/keys/SPAWN-CONSUL/name | jq '.node.value' | sed 's/.//;s/.$//')`
+ConsulVMNameK=`(curl http://127.0.0.1:4001/v2/keys/SPAWN-CONSUL/name | jq '.node.value' | sed 's/.//;s/.$//')`
 
 #spawn-receiver
 ReceiverNameK=`(curl http://127.0.0.1:4001/v2/keys/spawn-receiver/name | jq '.node.value' | sed 's/.//;s/.$//')`
@@ -72,7 +72,7 @@ while [ $i -lt $prevawsvms ]
 do
     #Reads VM Name from etcd
     
-    VMKill=`(http://127.0.0.1:4001/v2/keys/DM-AWS-$i/name | jq '.node.value' | sed 's/.//;s/.$//')`
+    VMKill=`(curl http://127.0.0.1:4001/v2/keys/DM-AWS-$i/name | jq '.node.value' | sed 's/.//;s/.$//')`
     #http://127.0.0.1:4001/v2/keys/DM-AWS-$i/name -XPUT
     #echo Provisioning VM SPAWN$i-$UUIDK
     echo ""
