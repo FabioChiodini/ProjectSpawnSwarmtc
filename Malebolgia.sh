ad46fe9898d5b1bc295828f47e35963a97aeebc8
Fabio Chiodini <FabioChiodini@users.noreply.github.com>
2016-04-19 14:59:53 +0200

    Update Malebolgia.sh
    indinstancek

--- Malebolgia.sh
+++ Malebolgia.sh
@@ -33,6 +33,7 @@ prevhoneypots=`(curl http://127.0.0.1:4001/v2/keys/totalhoneypots | jq '.node.va
 #swarm-master
 publicipSWARMK=`(curl http://127.0.0.1:4001/v2/keys/swarm-master/ip | jq '.node.value' | sed 's/.//;s/.$//')`
 SwarmTokenK=`(curl http://127.0.0.1:4001/v2/keys/swarm-master/token | jq '.node.value' | sed 's/.//;s/.$//')`
+SwarmVMName=`(curl http://127.0.0.1:4001/v2/keys/swarm-master/name | jq '.node.value' | sed 's/.//;s/.$//')`
 
 #SPAWN_CONSUL
 ConsulVMNameK=`(curl http://127.0.0.1:4001/v2/keys/consul/name | jq '.node.value' | sed 's/.//;s/.$//')`
@@ -40,7 +41,8 @@ ConsulVMNameK=`(curl http://127.0.0.1:4001/v2/keys/consul/name | jq '.node.value
 #spawn-receiver
 ReceiverNameK=`(curl http://127.0.0.1:4001/v2/keys/spawn-receiver/name | jq '.node.value' | sed 's/.//;s/.$//')`
 
-
+#etcd
+$etcdbrowserkVMName=`(curl http://127.0.0.1:4001/v2/keys/etcd-browser/name | jq '.node.value' | sed 's/.//;s/.$//')`
 
 #Kill Docker Machines
 echo ""
@@ -50,13 +52,15 @@ echo ""
 j=0
 while [ $j -lt $prevgcevms ]
 do
+   #Reads VM Name from etcd
+   VMKill=`(curl http://127.0.0.1:4001/v2/keys/DM-GCE-$i/name | jq '.node.value' | sed 's/.//;s/.$//')`
    echo ""
-   echo "Destroying VM env-crate-$j "
+   echo "Destroying VM $VMKill "
    echo ""
-   docker-machine rm -f env-crate-$j
+   docker-machine rm -f $VMKill
 
    echo ----
-   echo "$(tput setaf 1) Machine env-crate-$j Destroyed $(tput sgr 0)"
+   echo "$(tput setaf 1) Machine $VMKill Destroyed $(tput sgr 0)"
    echo ----
    #Increments counter for total GCE VMs
    true $(( j++ ))
@@ -91,10 +95,10 @@ do
 echo ""
 echo "$(tput setaf 1) Destroying Infrastructure Containers $(tput sgr 0)"
 echo ""
-docker-machine rm -f swarm-master
-docker-machine rm -f SPAWN-CONSUL
-docker-machine rm -f spawn-receiver
-docker-machine rm -f etcd-browserk
+docker-machine rm -f $SwarmVMName
+docker-machine rm -f $ConsulVMNameK
+docker-machine rm -f $ReceiverNameK
+docker-machine rm -f $etcdbrowserkVMName
 
 #Kill local containers
 echo ""
